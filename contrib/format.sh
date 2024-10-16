fb09c61a7db810c7d0e4a4e9bec193368190a515
wtz <wangtz0607@outlook.com>
2024-10-16 12:19:24 +0800

    Cleanup

--- contrib/format.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-script_dir="$(cd -- "$(dirname -- "$0")" && pwd -P)"
-exec "$script_dir/python.sh" "$script_dir/format.py" "$@"
--- contrib/python.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-if [[ -z "$PYTHON_COMMAND" ]]; then
-    PYTHON_COMMAND="$(which python3 || which python)"
-fi
-
-exec $PYTHON_COMMAND "$@"
