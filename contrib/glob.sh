557d60f09925aee553da505a9a0b1ddedef8fdb7
wtz <wangtz0607@outlook.com>
2024-10-19 13:05:22 +0800

    Add contrib/glob.sh

---
+++ contrib/glob.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eo pipefail
+
+git ls-files --cached --others --exclude-standard -z | while IFS= read -r -d '' file; do
+    if [[ "$(find "$file" \( -name '*.cpp' -o -name '*.h' \) -not -path 'third_party/*')" ]]; then
+        echo "$file"
+    fi
+done
