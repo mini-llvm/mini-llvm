56e2a6144e2b6ebf7c45bea4273be84815fd8ae6
wtz <wangtz0607@outlook.com>
2024-10-20 11:50:52 +0800

    Update contrib/pre-push.sh

--- contrib/pre-push.sh
+++ contrib/pre-push.sh
@@ -6,7 +6,7 @@ current_branch="$(git rev-parse --abbrev-ref HEAD)"
 
 if [[ "$current_branch" == "main" ]]; then
     if ! git merge-base --is-ancestor main origin/develop; then
-        echo "pre-push: error: 'main' cannot be ahead of or diverge from 'origin/develop'"
+        echo "pre-push: error: 'main' cannot be ahead of or diverge from 'origin/develop'" >&2
         exit 1
     fi
 fi
