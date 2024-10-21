80ee461585564f7ada435eda031fbd233ebb6f8b
wtz <wangtz0607@outlook.com>
2024-10-21 11:50:15 +0800

    Update contrib/pre-push.sh

--- contrib/pre-push.sh
+++ contrib/pre-push.sh
@@ -6,7 +6,8 @@ current_branch="$(git rev-parse --abbrev-ref HEAD)"
 
 if [[ "$current_branch" == "main" ]]; then
     if ! git merge-base --is-ancestor main origin/develop; then
-        echo "pre-push: error: 'main' cannot be ahead of or diverge from 'origin/develop'" >&2
+        echo "pre-push: 'main' cannot be ahead of or diverge from 'origin/develop'" >&2
+        echo "pre-push: aborting push"
         exit 1
     fi
 fi
