97a0edb23760dd22ab1063b3f908285127831aa0
wtz <wangtz0607@outlook.com>
2024-11-30 09:07:51 +0800

    [mini-llc] Add default value for <target>

--- tools/mini-llc/main.cpp
+++ tools/mini-llc/main.cpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <filesystem>
 #include <string>
+#include <string_view>
 
 #include <errno.h>
 #include <error.h>
@@ -22,6 +23,7 @@
 #include "mini-llvm/opt/ir/passes/VerificationAnalysis.h"
 #include "mini-llvm/opt/ir/PassManager.h"
 #include "mini-llvm/targets/riscv/RISCVBackendDriver.h"
+#include "mini-llvm/utils/ProcessorDetection.h"
 
 using namespace mini_llvm;
 
@@ -32,10 +34,17 @@ enum class Target {
     kRISCV64,
 };
 
+Target toTarget(std::string_view targetName) {
+    if (targetName == "riscv64") {
+        return Target::kRISCV64;
+    }
+    return Target::kNone;
+}
+
 struct Options {
-    Target target;
     std::filesystem::path inputFile;
     std::filesystem::path outputFile;
+    Target target;
 };
 
 void computeLineColumn(const char *start, const char *location, size_t &line, size_t &column) {
@@ -67,16 +76,17 @@ int main(int argc, char *argv[]) {
     while ((opt = getopt_long(argc, argv, shortOpts, longOpts, nullptr)) != -1) {
         switch (opt) {
         case CHAR_MAX + 1:
-            fprintf(stdout, "Usage: %s --target=<target> [-o <output-file>] <input-file>\n", argv[0]);
+            fprintf(stdout, "Usage: %s [--target=<target>] [-o <output-file>] <input-file>\n", argv[0]);
             exit(0);
 
         case CHAR_MAX + 2:
             if (options.target == Target::kNone) {
-                if (strcmp(optarg, "riscv64") == 0) {
-                    options.target = Target::kRISCV64;
+                Target target = toTarget(optarg);
+                if (target != Target::kNone) {
+                    options.target = target;
                     break;
                 }
-                fprintf(stderr, "%s: error: invalid target '%s'\n", argv[0], optarg);
+                fprintf(stderr, "%s: error: unsupported target '%s'\n", argv[0], optarg);
                 exit(1);
             }
             fprintf(stderr, "%s: error: multiple targets\n", argv[0]);
@@ -112,11 +122,6 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    if (options.target == Target::kNone) {
-        fprintf(stderr, "%s: error: no target\n", argv[0]);
-        exit(1);
-    }
-
     if (options.inputFile.empty()) {
         fprintf(stderr, "%s: error: no input file\n", argv[0]);
         exit(1);
@@ -124,6 +129,35 @@ int main(int argc, char *argv[]) {
 
     if (options.outputFile.empty()) {
         options.outputFile = std::filesystem::path(options.inputFile).replace_extension(".s");
+    }
+
+    if (options.target == Target::kNone) {
+        const char *targetName;
+#if defined(MINI_LLVM_X86_64)
+        targetName = "x86_64";
+#elif defined(MINI_LLVM_I386)
+        targetName = "i386";
+#elif defined(MINI_LLVM_AARCH64)
+        targetName = "aarch64";
+#elif defined(MINI_LLVM_ARM)
+        targetName = "arm";
+#elif defined(MINI_LLVM_PPC64)
+        targetName = "ppc64";
+#elif defined(MINI_LLVM_PPC)
+        targetName = "ppc";
+#elif defined(MINI_LLVM_RISCV64)
+        targetName = "riscv64";
+#elif defined(MINI_LLVM_RISCV32)
+        targetName = "riscv32";
+#else
+        targetName = "unknown";
+#endif
+        Target target = toTarget(targetName);
+        if (target != Target::kNone) {
+            options.target = target;
+        }
+        fprintf(stderr, "%s: error: unsupported target '%s'\n", argv[0], targetName);
+        exit(1);
     }
 
     int inputFd;
