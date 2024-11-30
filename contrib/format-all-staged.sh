45314e46be9ed9799bec224a830fde9e44800fce
wtz <wangtz0607@outlook.com>
2024-11-30 18:39:46 +0800

    Rename contrib/staged-sources.sh to sources-staged.sh

--- contrib/format-all-staged.sh
+++ contrib/format-all-staged.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 script_dir="$(cd -- "$(dirname -- "$0")" && pwd -P)"
-"$script_dir/staged-sources.sh" | xargs "$script_dir/format.py" "$@"
+"$script_dir/sources-staged.sh" | xargs "$script_dir/format.py" "$@"
