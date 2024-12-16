d6e0f61ae1e105d8c0dfebfa9ade80de3c0a1490
wtz <wangtz0607@outlook.com>
2024-12-16 18:04:50 +0800

    [test][mini-llc] Preserve result directory

--- tests/tools/mini-llc/test.sh
+++ tests/tools/mini-llc/test.sh
@@ -4,13 +4,13 @@ set -eu
 
 function run_test {
     local test_name="$1"
-    local temp_dir="$2"
+    local result_dir="$2"
 
-    mkdir -p "$(dirname "$temp_dir/$test_name")" &&
-    timeout -v "$MINI_LLC_TIMEOUT" $MINI_LLC_COMMAND --target="$TARGET" -o "$temp_dir/$test_name.s" "$test_name.ll" &&
-    timeout -v "$LINKER_TIMEOUT" $LINKER_COMMAND -o "$temp_dir/$test_name" "$temp_dir/$test_name.s" -lm &&
-    timeout -v "$EMULATOR_TIMEOUT" $EMULATOR_COMMAND "$temp_dir/$test_name" > "$temp_dir/$test_name.out" &&
-    $DIFF_COMMAND "${test_name%+*}.ans" "$temp_dir/$test_name.out"
+    mkdir -p "$(dirname "$result_dir/$test_name")" &&
+    timeout -v "$MINI_LLC_TIMEOUT" $MINI_LLC_COMMAND --target="$TARGET" -o "$result_dir/$test_name.s" "$test_name.ll" &&
+    timeout -v "$LINKER_TIMEOUT" $LINKER_COMMAND -o "$result_dir/$test_name" "$result_dir/$test_name.s" -lm &&
+    timeout -v "$EMULATOR_TIMEOUT" $EMULATOR_COMMAND "$result_dir/$test_name" > "$result_dir/$test_name.out" &&
+    $DIFF_COMMAND "${test_name%+*}.ans" "$result_dir/$test_name.out"
 }
 
 function main {
@@ -23,9 +23,11 @@ function main {
     LINKER_TIMEOUT="${LINKER_TIMEOUT:-10}"
     EMULATOR_TIMEOUT="${EMULATOR_TIMEOUT:-10}"
 
-    local temp_dir
-    temp_dir="$(mktemp -d)"
-    trap 'rm -rf "$temp_dir"' RETURN
+    local result_dir
+    result_dir="result_$(date "+%Y%m%d_%H%M%S")"
+
+    mkdir -p "$result_dir"
+    echo "*" > "$result_dir/.gitignore"
 
     local tests=("$@")
     local failed_tests=()
@@ -33,7 +35,7 @@ function main {
     for test_name in "${tests[@]}"; do
         printf "%-40s" "$test_name"
 
-        if run_test "$test_name" "$temp_dir"; then
+        if run_test "$test_name" "$result_dir"; then
             echo "Passed"
         else
             echo "Failed"
@@ -43,7 +45,7 @@ function main {
 
     if (( "${#failed_tests[@]}" > 0 )); then
         for test_name in "${failed_tests[@]}"; do
-            echo "$test_name"
+            echo "$test_name" >> "$result_dir/failed_tests.txt"
         done
         return 1
     fi
