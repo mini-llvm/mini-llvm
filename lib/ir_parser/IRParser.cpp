ef0b02ae73034e8d0109691361d35c8be980aaf2
wtz <wangtz0607@outlook.com>
2024-12-21 00:34:01 +0800

    [ir_parser] Fix memory issue in `parseModule`

--- lib/ir_parser/IRParser.cpp
+++ lib/ir_parser/IRParser.cpp
@@ -7,24 +7,30 @@
 #include "mini-llvm/ir/Module.h"
 #include "mini-llvm/ir_parser/Lexer.h"
 #include "mini-llvm/ir_parser/Parser.h"
+#include "mini-llvm/ir_parser/Token.h"
 #include "mini-llvm/utils/Strings.h"
 
 using namespace mini_llvm;
 using namespace mini_llvm::ir;
 
 std::optional<Module> ir::parseModule(const char *source, std::vector<Diagnostic> &diags) {
+    std::vector<Token> tokens;
+    Module M;
     try {
-        return parseModule(lex(source));
+        tokens = lex(source);
+        M = parseModule(tokens);
+        return M;
     } catch (const LexException &e) {
         size_t line, column;
         computeLineColumn(source, e.location(), line, column);
         diags.push_back(Diagnostic(Diagnostic::Severity::kError, e.message(), "<source>", line, column));
+        return std::nullopt;
     } catch (const ParseException &e) {
         size_t line, column;
         computeLineColumn(source, e.location()->location, line, column);
         diags.push_back(Diagnostic(Diagnostic::Severity::kError, e.message(), "<source>", line, column));
+        return std::nullopt;
     }
-    return std::nullopt;
 }
 
 std::optional<Module> ir::parseModule(const char *source) {
