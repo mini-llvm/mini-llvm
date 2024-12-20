97dc4489488d21bf6b4272c46bd9ab4cbcf6244a
wtz <wangtz0607@outlook.com>
2024-12-20 22:15:10 +0800

    Update contrib/sources-staged.sh

--- contrib/sources-staged.sh
+++ contrib/sources-staged.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git diff --cached --name-only | grep -E '\.cpp$|\.h$' | grep -v '^third_party/'
+git diff --cached --name-only --diff-filter=ACM | grep -E '\.cpp$|\.h$' | grep -v '^third_party/'
