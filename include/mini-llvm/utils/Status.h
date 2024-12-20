9301e413537106294c6ee77c202266907ae15091
wtz <wangtz0607@outlook.com>
2024-12-20 14:05:14 +0800

    [utils] Add `Expected` and `Status`

---
+++ include/mini-llvm/utils/Status.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <optional>
+#include <utility>
+
+namespace mini_llvm {
+
+struct Ok {};
+
+template <typename E>
+class Status {
+public:
+    using error_type = E;
+
+    constexpr Status(Ok) : error_(std::nullopt) {}
+
+    constexpr Status(E error) : error_(std::move(error)) {}
+
+    constexpr bool ok() const {
+        return !error_.has_value();
+    }
+
+    constexpr E &error() {
+        return error_.value();
+    }
+
+    constexpr const E &error() const {
+        return error_.value();
+    }
+
+private:
+    std::optional<E> error_;
+};
+
+} // namespace mini_llvm
