35ede7de1ac2272431600de779fc1bab44e6eea4
wtz <wangtz0607@outlook.com>
2024-12-31 19:30:35 +0800

    [utils] `ScopeGuard`: Add `dismiss()`

--- include/mini-llvm/utils/ScopeGuard.h
+++ include/mini-llvm/utils/ScopeGuard.h
@@ -29,19 +29,27 @@ public:
     explicit ScopeGuard(F &&f, Args &&...args)
         : f_(Wrapper<std::decay_t<F>, std::tuple<std::decay_t<Args>...>>(
             std::forward<std::decay_t<F>>(f),
-            std::tuple<std::decay_t<Args>...>(std::forward<std::decay_t<Args>...>(args)...))) {}
+            std::tuple<std::decay_t<Args>...>(std::forward<std::decay_t<Args>...>(args)...))), dismissed_(false) {}
 
     ~ScopeGuard() {
-        f_();
+        if (!dismissed_) {
+            f_();
+        }
     }
 
     ScopeGuard(const ScopeGuard &) = delete;
+    ScopeGuard(ScopeGuard &&) = delete;
+
     ScopeGuard &operator=(const ScopeGuard &) = delete;
-    ScopeGuard(ScopeGuard &&) = default;
-    ScopeGuard &operator=(ScopeGuard &&) = default;
+    ScopeGuard &operator=(ScopeGuard &&) = delete;
+
+    void dismiss() {
+        dismissed_ = true;
+    }
 
 private:
     std::function<void ()> f_;
+    bool dismissed_;
 };
 
 } // namespace mini_llvm
