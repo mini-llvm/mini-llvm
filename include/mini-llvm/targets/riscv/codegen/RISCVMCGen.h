70c2d9f62d4bc6fc06b27d240fee106a9d9b5d6f
wtz <wangtz0607@outlook.com>
2025-04-23 13:35:34 +0800

    Cleanup

--- include/mini-llvm/targets/riscv/codegen/RISCVMCGen.h
+++ include/mini-llvm/targets/riscv/codegen/RISCVMCGen.h
@@ -1,11 +1,7 @@
 #pragma once
 
-#include <string>
-#include <unordered_map>
-
 #include "mini-llvm/mc/Fragment.h"
 #include "mini-llvm/mc/Program.h"
-#include "mini-llvm/mir/BasicBlock.h"
 #include "mini-llvm/mir/Function.h"
 #include "mini-llvm/mir/GlobalVar.h"
 #include "mini-llvm/mir/Module.h"
@@ -21,7 +17,6 @@ public:
 private:
     const mir::Module *MM_;
     mc::Program *program_;
-    std::unordered_map<const mir::BasicBlock *, std::string> labelMap_;
 
     void emitGlobalVar(const mir::GlobalVar &G, mc::Fragment &fragment);
     void emitFunction(const mir::Function &F, mc::Fragment &fragment);
