069ba28886696d51aaf9c71b3ee0848908ca3d5b
wtz <wangtz0607@outlook.com>
2025-05-03 21:06:53 +0800

    Cleanup

--- lib/opt/ir/passes/InstructionCombining.cpp
+++ lib/opt/ir/passes/InstructionCombining.cpp
@@ -27,9 +27,9 @@ void dfs(const DominatorTreeNode *node, bool &changed) {
             std::shared_ptr<Value> lhs = share(*add->lhs()),
                                    rhs = share(*add->rhs());
             while (dynamic_cast<const Add *>(&*lhs) && dynamic_cast<const Constant *>(&*static_cast<const Add *>(&*lhs)->rhs())) {
-                Add *add = static_cast<Add *>(&*lhs);
-                lhs = share(*add->lhs());
-                rhs = Add(rhs, share(*add->rhs())).fold();
+                Add *add2 = static_cast<Add *>(&*lhs);
+                lhs = share(*add2->lhs());
+                rhs = Add(rhs, share(*add2->rhs())).fold();
             }
             if (&*lhs != &*add->lhs()) {
                 std::shared_ptr<Instruction> II = std::make_shared<Add>(lhs, rhs);
@@ -45,9 +45,9 @@ void dfs(const DominatorTreeNode *node, bool &changed) {
             std::shared_ptr<Value> lhs = share(*mul->lhs()),
                                    rhs = share(*mul->rhs());
             while (dynamic_cast<const Mul *>(&*lhs) && dynamic_cast<const Constant *>(&*static_cast<const Mul *>(&*lhs)->rhs())) {
-                Mul *mul = static_cast<Mul *>(&*lhs);
-                lhs = share(*mul->lhs());
-                rhs = Mul(rhs, share(*mul->rhs())).fold();
+                Mul *mul2 = static_cast<Mul *>(&*lhs);
+                lhs = share(*mul2->lhs());
+                rhs = Mul(rhs, share(*mul2->rhs())).fold();
             }
             if (&*lhs != &*mul->lhs()) {
                 std::shared_ptr<Instruction> II = std::make_shared<Mul>(lhs, rhs);
