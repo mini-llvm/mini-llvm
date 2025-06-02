9b45023fbc450666f94750c790bd252ca957cfb8
wtz <wangtz0607@outlook.com>
2025-06-02 21:17:21 +0800

    Cleanup

--- include/mini-llvm/ir/Type/FunctionType.h
+++ include/mini-llvm/ir/Type/FunctionType.h
@@ -3,7 +3,6 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstdlib>
-#include <iterator>
 #include <memory>
 #include <string>
 #include <utility>
@@ -12,66 +11,13 @@
 #include "mini-llvm/ir/Constant.h"
 #include "mini-llvm/ir/Type.h"
 #include "mini-llvm/ir/TypeVisitor.h"
+#include "mini-llvm/utils/IndirectIterator.h"
 
 namespace mini_llvm::ir {
 
 class FunctionType final : public Type {
 public:
-    class param_type_iterator {
-    public:
-        using iterator_category = std::random_access_iterator_tag;
-        using value_type = Type;
-        using difference_type = ptrdiff_t;
-
-    public:
-        param_type_iterator() = default;
-        Type &operator*() const { return **base_; }
-        Type &operator[](difference_type n) const { return *base_[n]; }
-        param_type_iterator &operator++() { ++base_; return *this; }
-        param_type_iterator &operator--() { --base_; return *this; }
-        param_type_iterator operator++(int) { return param_type_iterator(base_++); }
-        param_type_iterator operator--(int) { return param_type_iterator(base_--); }
-        param_type_iterator &operator+=(difference_type n) { base_ += n; return *this; }
-        param_type_iterator &operator-=(difference_type n) { base_ -= n; return *this; }
-
-        friend bool operator==(const param_type_iterator &lhs, const param_type_iterator &rhs) {
-            return lhs.base_ == rhs.base_;
-        }
-
-        friend bool operator!=(const param_type_iterator &lhs, const param_type_iterator &rhs) {
-            return lhs.base_ != rhs.base_;
-        }
-
-        friend bool operator<(const param_type_iterator &lhs, const param_type_iterator &rhs) {
-            return lhs.base_ < rhs.base_;
-        }
-
-        friend bool operator>(const param_type_iterator &lhs, const param_type_iterator &rhs) {
-            return lhs.base_ > rhs.base_;
-        }
-
-        friend bool operator<=(const param_type_iterator &lhs, const param_type_iterator &rhs) {
-            return lhs.base_ <= rhs.base_;
-        }
-
-        friend bool operator>=(const param_type_iterator &lhs, const param_type_iterator &rhs) {
-            return lhs.base_ >= rhs.base_;
-        }
-
-        friend difference_type operator-(const param_type_iterator &lhs, const param_type_iterator &rhs) {
-            return lhs.base_ - rhs.base_;
-        }
-
-        friend param_type_iterator operator+(param_type_iterator i, difference_type n) { return i += n; }
-        friend param_type_iterator operator-(param_type_iterator i, difference_type n) { return i -= n; }
-
-    private:
-        std::vector<std::unique_ptr<Type>>::const_iterator base_;
-
-        explicit param_type_iterator(std::vector<std::unique_ptr<Type>>::const_iterator base) : base_(base) {}
-
-        friend class FunctionType;
-    };
+    using param_type_iterator = IndirectIterator<std::vector<std::unique_ptr<Type>>::const_iterator, Type>;
 
     FunctionType(std::unique_ptr<Type> returnType, std::vector<std::unique_ptr<Type>> paramTypes, bool isVarArgs)
         : returnType_(std::move(returnType)), paramTypes_(std::move(paramTypes)), isVarArgs_(isVarArgs) {}
