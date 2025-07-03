8fe6f9d613daf3b6be79ec13dc0fbc442aea6573
wtz <wangtz0607@outlook.com>
2025-07-03 14:24:53 +0800

    [utils] Windows: Use std::string_view in widen() and narrow()

--- include/mini-llvm/utils/Windows.h
+++ include/mini-llvm/utils/Windows.h
@@ -3,24 +3,15 @@
 #ifdef _WIN32
 
 #include <string>
+#include <string_view>
 #include <vector>
 
 namespace mini_llvm::windows {
 
 // https://utf8everywhere.org/#windows
 
-std::wstring widen(const char *str);
-
-inline std::wstring widen(const std::string &str) {
-    return widen(str.c_str());
-}
-
-std::string narrow(const wchar_t *wstr);
-
-inline std::string narrow(const std::wstring &wstr) {
-    return narrow(wstr.c_str());
-}
-
+std::wstring widen(std::string_view str);
+std::string narrow(std::wstring_view wstr);
 std::vector<std::string> narrowArgs(int argc, wchar_t *wargv[]);
 
 } // namespace mini_llvm::windows
--- lib/utils/Windows.cpp
+++ lib/utils/Windows.cpp
@@ -3,29 +3,22 @@
 #include "mini-llvm/utils/Windows.h"
 
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include <windows.h>
 
-std::wstring mini_llvm::windows::widen(const char *str) {
-    if (*str == '\0') {
-        return {};
-    }
-    int size = MultiByteToWideChar(CP_UTF8, 0, str, -1, nullptr, 0);
+std::wstring mini_llvm::windows::widen(std::string_view str) {
+    int size = MultiByteToWideChar(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), nullptr, 0);
     std::wstring wstr(size, L'\0');
-    MultiByteToWideChar(CP_UTF8, 0, str, -1, wstr.data(), size);
-    wstr.pop_back();
+    MultiByteToWideChar(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), wstr.data(), size);
     return wstr;
 }
 
-std::string mini_llvm::windows::narrow(const wchar_t *wstr) {
-    if (*wstr == L'\0') {
-        return {};
-    }
-    int size = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, nullptr, 0, nullptr, nullptr);
+std::string mini_llvm::windows::narrow(std::wstring_view wstr) {
+    int size = WideCharToMultiByte(CP_UTF8, 0, wstr.data(), static_cast<int>(wstr.size()), nullptr, 0, nullptr, nullptr);
     std::string str(size, '\0');
-    WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str.data(), size, nullptr, nullptr);
-    str.pop_back();
+    WideCharToMultiByte(CP_UTF8, 0, wstr.data(), static_cast<int>(wstr.size()), str.data(), size, nullptr, nullptr);
     return str;
 }
 
