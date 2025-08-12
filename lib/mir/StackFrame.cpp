033ad863cd8176f6110a34348fecbeb222cd747f
wtz <wangtz0607@outlook.com>
2025-08-12 12:13:08 +0800

    Cleanup

--- lib/mir/StackFrame.cpp
+++ lib/mir/StackFrame.cpp
@@ -9,17 +9,17 @@ using namespace mini_llvm::mir;
 
 StackSlot &StackFrame::add(StackFrame::const_iterator pos, std::unique_ptr<StackSlot> slot) {
     offsetsUpToDate_ = false;
-    StackSlot &addedArea = **slots_.insert(pos.base(), std::move(slot));
-    addedArea.frame_ = this;
-    return addedArea;
+    StackSlot &addedSlot = **slots_.insert(pos.base(), std::move(slot));
+    addedSlot.frame_ = this;
+    return addedSlot;
 }
 
 std::unique_ptr<StackSlot> StackFrame::remove(StackFrame::iterator pos) {
     offsetsUpToDate_ = false;
-    std::unique_ptr<StackSlot> removedArea = std::move(*pos.base());
-    removedArea->frame_ = nullptr;
+    std::unique_ptr<StackSlot> removedSlot = std::move(*pos.base());
+    removedSlot->frame_ = nullptr;
     slots_.erase(pos.base());
-    return removedArea;
+    return removedSlot;
 }
 
 void StackFrame::clear() {
