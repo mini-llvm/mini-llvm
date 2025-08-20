38e96e36c9373167691935a83ffe23f68b71d585
wtz <wangtz0607@outlook.com>
2025-08-20 15:48:54 +0800

    Fix formatting scripts

--- contrib/format-staged.sh
+++ contrib/format-staged.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-git diff --cached --name-only --diff-filter=ACM | grep -v '^third_party/' | grep -E '\.c$|\.cpp$|\.h$' | xargs "$(dirname "$0")/format.py" "$@"
-git diff --cached --name-only --diff-filter=ACM | grep -v '^third_party/' | grep -E '(^|/)CMakeLists.txt$|\.cmake$' | xargs "$(dirname "$0")/cmake-format.py" "$@"
+git diff --cached --name-only --diff-filter=ACMR | grep -v '^third_party/' | grep -E '\.c$|\.cpp$|\.h$' | xargs "$(dirname "$0")/format.py" "$@"
+git diff --cached --name-only --diff-filter=ACMR | grep -v '^third_party/' | grep -E '(^|/)CMakeLists\.txt$|\.cmake$' | xargs "$(dirname "$0")/cmake-format.py" "$@"
