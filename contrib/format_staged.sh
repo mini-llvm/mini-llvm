9ae320e46b5cce1255991b98ede8c3c518424e18
wtz <wangtz0607@outlook.com>
2025-08-24 12:54:48 +0800

    Cleanup

--- contrib/format_staged.sh
+++ contrib/format_staged.sh
@@ -2,5 +2,12 @@
 
 set -e
 
-git diff --cached --name-only --diff-filter=ACMR | grep -v '^third_party/' | grep -E '\.c$|\.cpp$|\.h$' | xargs "$(dirname "$0")/format.py" "$@"
-git diff --cached --name-only --diff-filter=ACMR | grep -v '^third_party/' | grep -E '(^|/)CMakeLists\.txt$|\.cmake$' | xargs "$(dirname "$0")/cmake_format.py" "$@"
+git diff --cached --name-only --diff-filter=ACMR |
+  grep -v '^third_party/' |
+  grep -E '\.c$|\.cpp$|\.h$' |
+  xargs "$(dirname "$0")/format.py" "$@"
+
+git diff --cached --name-only --diff-filter=ACMR |
+  grep -v '^third_party/' |
+  grep -E '(^|/)CMakeLists\.txt$|\.cmake$' |
+  xargs "$(dirname "$0")/cmake_format.py" "$@"
