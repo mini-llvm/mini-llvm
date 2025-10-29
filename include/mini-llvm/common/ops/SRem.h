017ee38d0fa94355a83d61fc9bf118557e576e13
wtz <wangtz0607@outlook.com>
2025-10-29 20:16:04 +0800

    [common/ops] SRem: Fix integer overflow

--- include/mini-llvm/common/ops/SRem.h
+++ include/mini-llvm/common/ops/SRem.h
@@ -4,6 +4,7 @@
 
 #include <bit>
 #include <concepts>
+#include <limits>
 #include <optional>
 #include <type_traits>
 
@@ -13,12 +14,13 @@ struct SRem {
     template <typename T>
         requires std::integral<T>
     std::optional<T> operator()(T x, T y) const noexcept {
-        if (y == 0)
+        std::make_signed_t<T> sx = std::bit_cast<std::make_signed_t<T>>(x),
+                              sy = std::bit_cast<std::make_signed_t<T>>(y);
+        if (sy == 0)
             return std::nullopt;
-        return
-            std::bit_cast<T>(
-                static_cast<std::make_signed_t<T>>(
-                    std::bit_cast<std::make_signed_t<T>>(x) % std::bit_cast<std::make_signed_t<T>>(y)));
+        if (sx == std::numeric_limits<std::make_signed_t<T>>::min() && sy == -1)
+            return std::nullopt;
+        return std::bit_cast<T>(static_cast<std::make_signed_t<T>>(sx % sy));
     }
 
     std::optional<bool> operator()(bool, bool y) const noexcept {
