673904a3a97ae48ac81b2b3e4224a66b157ced38
wtz <wangtz0607@outlook.com>
2026-01-06 20:02:28 +0800

    Test multiple register allocators in fuzzing

--- fuzz/fuzz.cpp
+++ fuzz/fuzz.cpp
@@ -2,10 +2,14 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <memory>
 #include <optional>
 #include <string>
 
 #include "mini-llvm/codegen/register_allocators/GraphColoringAllocator.h"
+#include "mini-llvm/codegen/register_allocators/LinearScanAllocator.h"
+#include "mini-llvm/codegen/register_allocators/NaiveAllocator.h"
+#include "mini-llvm/codegen/RegisterAllocator.h"
 #include "mini-llvm/ir/Module.h"
 #include "mini-llvm/ir_reader/IRReader.h"
 #include "mini-llvm/mc/Module.h"
@@ -17,11 +21,9 @@ using namespace mini_llvm;
 
 namespace {
 
-void testRISCV(ir::Module &IM) {
+void testRISCV(ir::Module &IM, RegisterAllocator &allocator) {
     ir::PassManager passManager(8);
     passManager.run(IM);
-
-    GraphColoringAllocator allocator;
 
     RISCVBackendDriver driver(&allocator);
 
@@ -34,7 +36,24 @@ void testRISCV(ir::Module &IM) {
 } // namespace
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-    std::string source(reinterpret_cast<const char *>(data), size);
+    if (size < 1) {
+        return 0;
+    }
+
+    std::unique_ptr<RegisterAllocator> allocator;
+    switch (*data % 3) {
+    case 0:
+        allocator = std::make_unique<GraphColoringAllocator>();
+        break;
+    case 1:
+        allocator = std::make_unique<LinearScanAllocator>();
+        break;
+    case 2:
+        allocator = std::make_unique<NaiveAllocator>();
+        break;
+    }
+
+    std::string source(reinterpret_cast<const char *>(data + 1), size - 1);
 
     std::optional<ir::Module> IM = ir::parseModule(source);
     if (!IM) {
@@ -44,7 +63,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         return 0;
     }
 
-    testRISCV(*IM);
+    testRISCV(*IM, *allocator);
 
     return 0;
 }
