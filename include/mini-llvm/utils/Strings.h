46b5c0236eab8c1435964e86902e12ba327a069e
wtz <wangtz0607@outlook.com>
2026-01-12 11:48:47 +0800

    Adjust overloads of toString()

--- include/mini-llvm/utils/Strings.h
+++ include/mini-llvm/utils/Strings.h
@@ -2,17 +2,18 @@
 
 #pragma once
 
-#include <cstdint>
 #include <string>
 
 #include "mini-llvm/utils/Compiler.h"
 
 namespace mini_llvm {
 
-MINI_LLVM_EXPORT std::string toString(int32_t value, int base = 10);
-MINI_LLVM_EXPORT std::string toString(int64_t value, int base = 10);
-MINI_LLVM_EXPORT std::string toString(uint32_t value, int base = 10);
-MINI_LLVM_EXPORT std::string toString(uint64_t value, int base = 10);
+MINI_LLVM_EXPORT std::string toString(int value, int base = 10);
+MINI_LLVM_EXPORT std::string toString(long value, int base = 10);
+MINI_LLVM_EXPORT std::string toString(long long value, int base = 10);
+MINI_LLVM_EXPORT std::string toString(unsigned int value, int base = 10);
+MINI_LLVM_EXPORT std::string toString(unsigned long value, int base = 10);
+MINI_LLVM_EXPORT std::string toString(unsigned long long value, int base = 10);
 
 MINI_LLVM_EXPORT void normalizeLineEndings(std::string &str);
 
