67338464ecedf46dc118687f769ee834eb639714
wtz <wangtz0607@outlook.com>
2026-01-22 19:52:44 +0800

    Update setup_hooks.sh

--- scripts/setup_hooks.sh
+++ scripts/setup_hooks.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-ln -sf ../../scripts/pre_commit.sh .git/hooks/pre-commit
-ln -sf ../../scripts/pre_push.sh .git/hooks/pre-push
+ln -sf ../../scripts/pre_commit.sh "$(dirname -- "$0")/../.git/hooks/pre-commit"
+ln -sf ../../scripts/pre_push.sh "$(dirname -- "$0")/../.git/hooks/pre-push"
